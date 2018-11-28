5f74472eb7f90d4326814745a9db33f2b6bf0fbb
Saqib Hussain <saqib.hussain_@hotmail.co.uk>
2018-11-28 21:56:49 +0000

    can now post via admin app

--- backend/src/costingapp/admin.py
+++ backend/src/costingapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from costingapp.models import TBLProduct
+
+admin.site.register(TBLProduct)
--- backend/src/costingapp/models.py
+++ backend/src/costingapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class TBLProduct(models.Model):
+    productid = models.IntegerField()
+    firstimpproductid = models.IntegerField()
+    fulltitle = models.CharField(max_length=255)
+    createddate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fulltitle
